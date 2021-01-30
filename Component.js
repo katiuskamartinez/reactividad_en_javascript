730d2fdf9a29d30cb3b4e590f5614bdba4aed2e9
Yohana <katiuskm.m@gmail.com>
2021-01-30 19:03:06 -0300

    clase libreria componentes con estado

---
+++ Component.js
@@ -0,0 +1,30 @@
+const Component = (function () {
+  //Creamos el constructor del Componente
+  const Constructor = function (options) {
+    this.el = options.el;
+    this.data = options.data;
+    this.template = options.template;
+  };
+  //Agregamos los métodos al prototipo del constructor del componente
+  // Render UI
+  Constructor.prototype.render = function () {
+    const $el = document.querySelector(this.el);
+    if (!$el) return;
+    $el.innerHTML = this.template(this.data);
+    console.log(this.data);
+  };
+  // Actualizar el State de forma reactiva
+  Constructor.prototype.setState = function (obj) {
+    for (let key in obj) {
+      if (this.data.hasOwnProperty(key)) {
+        this.data[key] = obj[key];
+      }
+    }
+    this.render();
+  };
+  // Obtenemos una copia inmutable del Estado
+  Constructor.prototype.getState = function () {
+    return JSON.parse(JSON.stringify(this.data));
+  };
+  return Constructor;
+})();
